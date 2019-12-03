e27835a928f43b90779e8d390cfd06710d0d8dde
pmpsilva <pmpsilva2@gmail.com>
2019-12-03 17:03:16 +0000

    initial commit clean build with dependencies

---
+++ src/main/java/com/pmpsilva/taxapi/TaxApiApplication.java
@@ -0,0 +1,13 @@
+package com.pmpsilva.taxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmpsilva/taxapi/TaxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmpsilva.taxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
